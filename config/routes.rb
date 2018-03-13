fef6bd64d64ac21b2e34eb58ea44c0163c245be7
Kelly Walters <kellywdev@gmail.com>
2018-03-13 23:35:41 +0000

    Part -2 add customer controller and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'customers/alphabetized'
+
+  get 'customers/missing_email'
+
+  resources :customers, only: [:index]
+
+  root to: 'customers#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
